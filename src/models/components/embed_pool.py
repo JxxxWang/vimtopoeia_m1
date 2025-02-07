66bbc97438615561b36ec6cb6314b40cd2425caa
Ben Hayes <b.j.hayes@qmul.ac.uk>
2025-02-07 16:57:17 +0000

    pos enc in embed dim

--- src/models/components/embed_pool.py
+++ src/models/components/embed_pool.py
@@ -14,7 +14,7 @@ class EmbeddingPool(nn.Module):
             d_model, num_heads, kdim=embed_dim, vdim=embed_dim, batch_first=True
         )
         self.query = nn.Parameter(torch.randn(1, 1, d_model))
-        self.positional_encoding = nn.Parameter(torch.zeros(1, 1, d_model))
+        self.positional_encoding = nn.Parameter(torch.zeros(1, 1, embed_dim))
 
     def forward(self, embed: torch.Tensor):
         embed = embed.permute(0, 2, 1)
