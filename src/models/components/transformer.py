dff5a697371de77f2197dfddcc29780e1f471c4c
Ben Hayes <b.j.hayes@qmul.ac.uk>
2025-02-07 21:08:54 +0000

    make input contig

--- src/models/components/transformer.py
+++ src/models/components/transformer.py
@@ -936,6 +936,7 @@ class AudioSpectrogramTransformer(nn.Module):
         x = self.positional_encoding(x)
 
         # apply transformer
+        x = x.contiguous()
         for block in self.blocks:
             x = block(x)
 
