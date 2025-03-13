2ef1c0a68263c3985465cb16315aa25e9fc4dc39
Ben Hayes <b.j.hayes@qmul.ac.uk>
2025-03-13 16:54:37 +0000

    add exps

---
+++ jobs/train/kosc/train.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -l h_rt=240:0:0
+#$ -l gpu=1
+#$ -cwd
+#$ -j y
+#$ -o qlogs/
+#$ -e qlogs/
+
+#$ -l rocky
+
+#$ -l cluster=andrena
+#$ -l h_vmem=7.5G
+#$ -pe smp 12
+
+# -l node_type=rdg
+# -l gpuhighmem
+# -l h_vmem=20G
+# -pe smp 12
+
+#$ -t 33-44
+
+EXPERIMENT=$(sed -n "${SGE_TASK_ID}p" jobs/train/kosc/experiments.txt)
+
+rm -rf ~/.triton/cache
+mamba activate perm
+module load gcc
+python src/train.py experiment=$EXPERIMENT \
+  seed=999
